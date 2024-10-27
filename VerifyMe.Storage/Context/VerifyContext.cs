75860b2dee54fa58ab5b36619d338824e79823bf
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-27 20:53:52 +0400

    Подключение Ef core + контекст подключения

---
+++ VerifyMe.Storage/Context/VerifyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerifyMe.Models.DLA;
+
+namespace VerifyMe.Storage.Context;
+
+public sealed class VerifyContext : DbContext
+{
+    public VerifyContext() => Database.MigrateAsync();
+    public DbSet<App> Apps { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Sms> Sms { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = app.db");
+    }
+}
