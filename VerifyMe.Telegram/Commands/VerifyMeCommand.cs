f70c1019bebac1d0b16fc10429b44e0ca1892487
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-27 22:00:51 +0400

    Реализация /start и обработки получение контактов в виде шаблона

---
+++ VerifyMe.Telegram/Commands/VerifyMeCommand.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using VerifyMe.Telegram.Common;
+using VerifyMe.Telegram.Extensions;
+
+namespace VerifyMe.Telegram.Commands;
+
+public class VerifyMeCommand : BaseCommand
+{
+    public override string Command { get; } = string.Empty;
+
+    private readonly string _successVerifyed =
+        "Спасибо, номер подтвержден. Теперь Вы сможете получать сообщения от онлайн-сервисов, которые подключены к этому боту.";
+
+    private readonly string _unSuccessVerifyed =
+        "К сожалению, Вы пытаетесь надурить. Как надумаете подтвердить номер отправьте команду /start";
+
+    public override async Task ExecuteAsync(ITelegramBotClient client, Message message)
+    {
+        if (message.From?.Id != message.Contact?.UserId)
+        {
+            await client.TrySendMessage(message.Chat.Id, _unSuccessVerifyed, replyMarkup: new ReplyKeyboardRemove());
+            return;
+        }
+
+        await client.TrySendMessage(message.Chat.Id, _successVerifyed, replyMarkup: new ReplyKeyboardRemove());
+    }
+}
--- VerifyMe.Telegram/UpdateHandler.cs
+++ VerifyMe.Telegram/UpdateHandler.cs
@@ -21,6 +21,12 @@ public class UpdateHandler(ILogger<UpdateHandler> logger) : IUpdateHandler
     {
         switch (update.Type)
         {
+            case UpdateType.Message when update.Message is { From: not null, Contact: not null }:
+            {
+                await new VerifyMeCommand().ExecuteAsync(botClient, update.Message);
+                break;
+            }
+            
             case UpdateType.Message when update.Message?.From != null:
             {
                 foreach (var command in _commands)
@@ -28,41 +34,28 @@ public class UpdateHandler(ILogger<UpdateHandler> logger) : IUpdateHandler
                     if(!command.Contains(update.Message))
                         continue;
                     
-                 //   logger.LogInformation($"Обработка команды: {command.Command}. от: ID {update.Message.From.Id} в чате: {update.Message.Chat.Id}");
+                    //   logger.LogInformation($"Обработка команды: {command.Command}. от: ID {update.Message.From.Id} в чате: {update.Message.Chat.Id}");
                     await command.ExecuteAsync(botClient, update.Message);
                 }
                 //logger.LogInformation($"Сообщение: {update.Message.MessageId}. от: ID {update.Message.From.Id} в чате: {update.Message.Chat.Id} c текстом: {update.Message.Text}");
                 break;
             }
             
+            
             case UpdateType.Unknown:
-                break;
             case UpdateType.InlineQuery:
-                break;
             case UpdateType.ChosenInlineResult:
-                break;
             case UpdateType.EditedMessage:
-                break;
             case UpdateType.ChannelPost:
-                break;
             case UpdateType.EditedChannelPost:
-                break;
             case UpdateType.ShippingQuery:
-                break;
             case UpdateType.PreCheckoutQuery:
-                break;
             case UpdateType.Poll:
-                break;
             case UpdateType.PollAnswer:
-                break;
             case UpdateType.MyChatMember:
-                break;
             case UpdateType.ChatMember:
-                break;
             case UpdateType.ChatJoinRequest:
                 break;
-            default:
-                throw new ArgumentOutOfRangeException();
         }
     }
     
