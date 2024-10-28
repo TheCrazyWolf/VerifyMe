685827befd8828594fd58991c5aac223d14fd3de
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-28 11:41:03 +0400

    Нормализатор телефона

---
+++ VerifyMe.Services/Extensions/PhoneNormalizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace VerifyMe.Services.Extensions;
+
+public static class PhoneNormalizer
+{
+    public static string GetNormalizedPhoneNumber(string phoneNumber)
+    {
+        phoneNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+        
+        // Заменяем первую цифру 8 на +7
+        if (phoneNumber.StartsWith("8"))
+            phoneNumber = "+7" + phoneNumber.Substring(1);
+        else if (phoneNumber.StartsWith("7"))
+            phoneNumber = "+" + phoneNumber;
+
+        return phoneNumber;
+    }
+}
