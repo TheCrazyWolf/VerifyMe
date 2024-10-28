15bd9a29b9c15f924a9253dcba7c2967c003e37e
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-28 09:24:15 +0400

    Добавил конструкторы в DTO

--- VerifyMe.Models/DTO/SmsResult.cs
+++ VerifyMe.Models/DTO/SmsResult.cs
@@ -4,4 +4,15 @@ public class SmsResult
 {
     public bool IsSuccess { get; set; }
     public string SystemMessage { get; set; } = string.Empty;
+
+    public SmsResult(bool isSuccess, string systemMessage)
+    {
+        IsSuccess = isSuccess;
+        SystemMessage = systemMessage;
+    }
+
+    public SmsResult()
+    {
+        
+    }
 }
