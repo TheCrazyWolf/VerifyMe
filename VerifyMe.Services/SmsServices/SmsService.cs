91a4596753ce452730bcd1f3ac886c42d1c3300e
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-28 12:28:21 +0400

    Валидация на пустоты сообщений

--- VerifyMe.Services/SmsServices/SmsService.cs
+++ VerifyMe.Services/SmsServices/SmsService.cs
@@ -13,6 +13,7 @@ public class SmsService(VerifyStorage storage, ITelegramBotClient botClient)
         phoneNumber = phoneNumber.GetNormalizedPhoneNumber();
         var user = await storage.Users.GetUserByPhone(phoneNumber);
         if (user == null) return new SmsResult(false, "Пользователь не зарегистрирован в сервисе");
+        if (string.IsNullOrEmpty(message)) return new SmsResult(false, "Сообщение не может быть пустым");
         message += $"\n\nℹ️ Вы получили это сообщение от сервиса: {app.Name}";
         var isSuccess = await botClient.TrySendMessage(user.Id, message);
         Sms sms = new Sms { AppId = app.Id, UserId = user.Id, Message = message, IsDelivered  = isSuccess, DateTimeSend = DateTime.Now};
