711d42d9b9a8d4ee2f86b25526db1b7839f736a0
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-28 23:33:44 +0400

    Перефразировал

--- VerifyMe.Services/SmsServices/SmsService.cs
+++ VerifyMe.Services/SmsServices/SmsService.cs
@@ -13,13 +13,13 @@ public class SmsService(VerifyStorage storage, ITelegramBotClient botClient)
     {
         phoneNumber = phoneNumber.GetNormalizedPhoneNumber();
         var user = await storage.Users.GetUserByPhone(phoneNumber);
-        if (user == null) return new SmsResult(false, "Пользователь не зарегистрирован в сервисе");
+        if (user == null) return new SmsResult(false, "Не удалось отправить сообщение. Пожалуйста перейдите, в телеграм бота и подтвердите свой номер телефона для получение смс-кодов.");
         if (string.IsNullOrEmpty(message)) return new SmsResult(false, "Сообщение не может быть пустым");
         message += $"\n\nℹ️ Вы получили это сообщение от сервиса: {app.Name}";
         var isSuccess = await botClient.TrySendMessage(user.Id, message);
         Sms sms = new Sms { AppId = app.Id, UserId = user.Id, Message = message, IsDelivered  = isSuccess, DateTimeSend = DateTime.Now};
         await storage.Sms.CreateSms(sms);
-        return new SmsResult(isSuccess, isSuccess ? "Сообщение доставлено" : "Пользователь удалил бота или телеграмм недоступен");
+        return new SmsResult(isSuccess, isSuccess ? "Сообщение успешно отправлено" : "Не удалость отправить сообщение. Пользователь деактивировал телеграм бота или проблемы с доступом к телеграму.");
     }
     
     public async Task<IList<SmsResultExtended>> SendSmsAsync(App app, IList<PostSendSms> dtos)
@@ -27,7 +27,7 @@ public class SmsService(VerifyStorage storage, ITelegramBotClient botClient)
         IList<SmsResultExtended> resultExtendeds = new List<SmsResultExtended>();
         foreach (var dto in dtos)
         {
-            var result =await SendSmsAsync(app, dto.Phone, dto.Message);
+            var result = await SendSmsAsync(app, dto.Phone, dto.Message);
             resultExtendeds.Add(new SmsResultExtended(result.IsSuccess, dto.Phone, result.SystemMessage));
             await Task.Delay(10000);
         }
