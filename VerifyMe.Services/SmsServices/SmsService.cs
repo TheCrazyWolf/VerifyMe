04ee32367ef718e4c05b9343d29f2fd512e12398
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-28 09:25:55 +0400

    Возращаемый тип SmsResult

--- VerifyMe.Services/SmsServices/SmsService.cs
+++ VerifyMe.Services/SmsServices/SmsService.cs
@@ -1,5 +1,6 @@
 ﻿using Telegram.Bot;
 using VerifyMe.Models.DLA;
+using VerifyMe.Models.DTO;
 using VerifyMe.Services.Extensions;
 using VerifyMe.Storage;
 
@@ -7,15 +8,15 @@ namespace VerifyMe.Services.SmsServices;
 
 public class SmsService(VerifyStorage storage, ITelegramBotClient botClient)
 {
-    public async Task<int> SendSmsAsync(App app, string phoneNumber, string message)
+    public async Task<SmsResult> SendSmsAsync(App app, string phoneNumber, string message)
     {
         var user = await storage.Users.GetUserByPhone(phoneNumber);
-        if (user == null) return 404;
+        if (user == null) return new SmsResult(false, "Пользователь не зарегистрирован в сервисе");
         message += $"\n\nℹ️ Вы получили это сообщение от сервиса: ID №{app.Id}. {app.Name}";
         var isSuccess = await botClient.TrySendMessage(user.Id, message);
         Sms sms = new Sms { AppId = app.Id, UserId = user.Id, Message = message, IsDelivered  = isSuccess, DateTimeSend = DateTime.Now};
         await storage.Sms.CreateSms(sms);
-        return 200;
+        return new SmsResult(true, "Сообщение отправлено");
     }
     
     public async Task SendSmsAsync(App app, IList<string> phoneNumbers, string message)
--- VerifyMe/Controllers/SmsController.cs
+++ VerifyMe/Controllers/SmsController.cs
@@ -10,16 +10,10 @@ namespace VerifyMe.Controllers;
 public class SmsController(SmsService smsService, AppsServices appsServices) : ControllerBase
 {
     [HttpPost("Send")]
-    public async Task<IActionResult> Send([FromHeader] string accessToken, [FromBody] PostSendSms dto)
+    public async Task<SmsResult> Send([FromHeader] string accessToken, [FromBody] PostSendSms dto)
     {
         var application = await appsServices.GetAppByAccessToken(accessToken);
-        if(application == null) return Unauthorized("Доступ запрещен. Проверьте передачу токена в заголовке AccessToken");
-
-        var resultInCode = await smsService.SendSmsAsync(application, dto.Phone, dto.Message);
-        
-        if(resultInCode is 200) return Ok("Сообщение отправлено");
-        if(resultInCode is 404) return NotFound("Пользователь не зарегистрирован. Перейдите в телеграм бот.");
-
-        return Ok();
+        if(application == null) return new SmsResult(false,"Доступ запрещен. Проверьте передачу токена в заголовке AccessToken");
+        return await smsService.SendSmsAsync(application, dto.Phone, dto.Message);
     }
 }
